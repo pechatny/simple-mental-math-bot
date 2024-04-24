668387e306b5d55cc899340c29a8d2a547bfa698
pechatny <pechatny@gmail.com>
2024-04-24 19:21:50 +0300

    implement simple int sum training

--- src/main/java/ru/pechatny/command/StartCommand.java
+++ src/main/java/ru/pechatny/command/StartCommand.java
@@ -1,11 +1,13 @@
 package ru.pechatny.command;
 
-import ru.pechatny.bot.Bot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.pechatny.bot.Bot;
 
-public class StartCommand implements Command{
-    private static final String START_MESSAGE = "start message text";
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class StartCommand implements Command {
     private final Bot bot;
     private final Long chatId;
 
@@ -17,11 +19,38 @@ public class StartCommand implements Command{
 
     @Override
     public void execute() throws TelegramApiException {
+        sendMessage(chatId, "Начало упражнения!");
+        Random random = new Random();
+        IntStream.rangeClosed(1, 10).forEach(i -> {
+            int a = random.nextInt(100);
+            int b = random.nextInt(100);
+            int result = a + b;
+            String task = "%d: %d + %d".formatted(i, a, b);
+            sendMessage(chatId, task);
+            try {
+                Thread.sleep(9000);
+
+                String resultMessage = "%d: %d + %d = %d".formatted(i, a, b, result);
+                sendMessage(chatId, resultMessage);
+
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+        });
+
+        sendMessage(chatId, "Упражнение закончено!");
+    }
+
+    public void sendMessage(Long chatId, String message) {
         SendMessage sendMessage = new SendMessage();
-
-        sendMessage.setText(START_MESSAGE);
+        sendMessage.setText(message);
         sendMessage.setChatId(chatId);
-        bot.execute(sendMessage);
+        try {
+            this.bot.execute(sendMessage);
+        } catch (TelegramApiException e) {
 
+        }
     }
 }

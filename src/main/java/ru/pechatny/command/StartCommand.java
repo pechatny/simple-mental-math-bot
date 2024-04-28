2daee397bc737933983d2ff84de180919d5c35f1
pechatny <pechatny@gmail.com>
2024-04-28 07:34:38 +0300

    fix number generation. only double digit numbers

--- src/main/java/ru/pechatny/command/StartCommand.java
+++ src/main/java/ru/pechatny/command/StartCommand.java
@@ -8,6 +8,8 @@ import java.util.Random;
 import java.util.stream.IntStream;
 
 public class StartCommand implements Command {
+    public static final int WAIT_BETWEEN_QUESTIONS = 4000;
+    public static final int WAIT_AFTER_ANSWER = 500;
     private final Bot bot;
     private final Long chatId;
 
@@ -22,18 +24,17 @@ public class StartCommand implements Command {
         sendMessage(chatId, "Начало упражнения!");
         Random random = new Random();
         IntStream.rangeClosed(1, 10).forEach(i -> {
-            int a = random.nextInt(100);
-            int b = random.nextInt(100);
+            int a = random.nextInt(11, 100);
+            int b = random.nextInt(11,100);
             int result = a + b;
             String task = "%d: %d + %d".formatted(i, a, b);
             sendMessage(chatId, task);
             try {
-                Thread.sleep(9000);
-
+                Thread.sleep(WAIT_BETWEEN_QUESTIONS);
                 String resultMessage = "*%d*".formatted(result);
                 sendMessage(chatId, resultMessage);
 
-                Thread.sleep(1000);
+                Thread.sleep(WAIT_AFTER_ANSWER);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }

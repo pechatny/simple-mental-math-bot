0b1754efec07e9a97488af145cbb8e2e5fb1f545
pechatny <pechatny@gmail.com>
2024-04-28 07:53:25 +0300

    fix start command

--- src/main/java/ru/pechatny/processor/MessageProcessorLogic.java
+++ src/main/java/ru/pechatny/processor/MessageProcessorLogic.java
@@ -1,7 +1,5 @@
 package ru.pechatny.processor;
 
-import org.slf4j.LoggerFactory;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import ru.pechatny.bot.Bot;
 import ru.pechatny.command.StartCommand;
 
@@ -12,13 +10,7 @@ public class MessageProcessorLogic implements MessageProcessor {
     }
 
     public void processMessage(Bot bot, Long chatId, String message) {
-        message = message.trim();
-
-        try {
-            var command = new StartCommand(bot, chatId);
-            command.execute();
-        } catch (TelegramApiException e) {
-            LoggerFactory.getLogger(MessageProcessorLogic.class).error("Message processing error! Message: {}", message);
-        }
+        var command = new StartCommand(bot, chatId);
+        command.execute();
     }
 }
